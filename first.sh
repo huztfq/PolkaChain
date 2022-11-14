59f777275397549edd062cfcfde29e63ff90cd88
Huzaifa Tofeeq <75849337+huztfq@users.noreply.github.com>
2022-11-15 00:58:53 +0500

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,1 +1,10 @@
-
+./target/release/node-template \
+--base-path /tmp/alice \
+--chain local \
+--alice \
+--port 30333 \
+--ws-port 9945 \
+--rpc-port 9933 \
+--node-key 0000000000000000000000000000000000000000000000000000000000000001 \
+--telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
+--validator
