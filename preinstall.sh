9d0c723b748ba3e150f62d4a59532005e202218d
Huzaifa Tofeeq <75849337+huztfq@users.noreply.github.com>
2022-11-14 17:19:09 +0500

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,14 +1,16 @@
-sudo apt-get update
-sudo apt install build-essential
-sudo apt-get -y install clang
-sudo apt install --assume-yes git clang curl libssl-dev
-sudo apt install --assume-yes git clang curl libssl-dev llvm libudev-dev make protobuf-compiler
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source $HOME/.cargo/env
+apt-get update
+apt install build-essential
+apt-get -y install clang
+apt install --assume-yes git clang curl libssl-dev
+apt install --assume-yes git clang curl libssl-dev llvm libudev-dev make protobuf-compiler
+echo "1" | curl https://sh.rustup.rs -sSf | bash -s -- -y
+export PATH="$HOME/.cargo/bin:$PATH"
 rustc --version
 rustup default stable
 rustup update
 rustup update nightly
 rustup target add wasm32-unknown-unknown --toolchain nightly
 rustup show
-rustup +nightly show
+rustup +nightly show
+rustup uninstall stable
+rustup install stable
