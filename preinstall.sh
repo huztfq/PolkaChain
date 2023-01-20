c312c07b2137d45d1e4552690b108e6fcd2bda2f
Huzaifa Tofeeq <75849337+huztfq@users.noreply.github.com>
2023-01-20 07:05:28 +0500

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
-apt-get update
-apt-get install net-tools
-apt install build-essential
-apt-get -y install clang
-apt install --assume-yes git clang curl libssl-dev
-apt install --assume-yes git clang curl libssl-dev llvm libudev-dev make protobuf-compiler
-echo "1" | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs 
-export PATH="$HOME/.cargo/bin:$PATH"
-rustc --version
-rustup default stable
-rustup update
-rustup update nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly
-rustup show
-rustup +nightly show
-rustup uninstall stable
-rustup install stable
-apt-get update
+
+# Update package list
+sudo apt-get update
+sudo apt-get install net-tools
+sudo apt install build-essentials
+sudo apt-get -y install clang
+# Install dependencies
+sudo apt-get install -y build-essential pkg-config libssl-dev
+sudo apt install --assume-yes git clang curl libssl-dev llvm libudev-dev make protobuf-compiler
+
+# Install Rust and Cargo
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+
+# Install Substrate
+cargo install --git https://github.com/paritytech/substrate.git
+
+# Verify installation
+substrate --version
+
+# Create a new substrate node
+substrate-node-new my-node
+
+# Change directory to the node's folder
+cd my-node
+
+# Build the node
+cargo build --release
